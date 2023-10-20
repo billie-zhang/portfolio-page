a8aac4defd118c88b8d5d9a8d3681579ca53dd62
Billie Zhang <billie.zhang1504@gmail.com>
2023-10-19 21:22:08 -0400

    update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-[1100px] sm:h-[900px] md:h-full lg:h-[700px] pt-[110px] bg-almost-black text-light-grey"
+      className="w-full h-full pt-[110px] bg-almost-black text-light-grey"
     >
       <div className="max-w-screen-lg p-6 mx-auto flex flex-col justify-center w-full h-auto">
         <div className="pb-8" data-aos="fade-up">
@@ -35,24 +35,48 @@ const About = () => {
         <br />
 
         <p className="text-l leading-relaxed" data-aos="fade-up">
-          In my current role as a Full Stack Developer at RBC, I am working with
-          React and Typescript applications, and using CSS to style for optimal
-          user experiences and accessibility. The main application I am working
-          on is a chatbot that is able to help RBC employees with their tech
-          inquiries, including laptop issues, ticket lookup, VPN lockout, and
-          much more. Additionally, I'm designing components of our applications
-          on Figma and implementing conversation flows to improve the user
-          experience.
+          In my current role as a <b>Full Stack Developer at RBC</b>, I am
+          working with React and Typescript applications, and using CSS to style
+          for optimal user experiences and accessibility. The main application I
+          am working on is a Tia, a chatbot that is able to help RBC employees
+          with their tech inquiries, including laptop issues, ticket lookup, VPN
+          lockout, and much more. Additionally, I'm designing components of our
+          applications on Figma and implementing new conversation flows to
+          improve the user experience.
         </p>
 
         <br />
 
         <p className="text-l leading-relaxed" data-aos="fade-up">
           Beyond learning and growing in the professional realm, I also enjoy
-          reading, baking, early morning runs, spending time with family and
-          friends, learning new languages, crocheting, learning music and being
-          outside with nature. Feel free to reach out if you have any questions
-          or just want to chat! I love to connect with like-minded individuals.
+          exploring many other hobbies that spark my creativity. I can easily
+          spend an entire day reading a book, immersed in a new journey. In the
+          real world, I love traveling, being able to explore diverse cultures
+          and landscapes. Both reading and traveling transport me to different
+          worlds, igniting my curiosity and fueling my passion for exploration.
+        </p>
+
+        <br />
+        {/* insert photo gallery  */}
+
+        <p className="text-l leading-relaxed" data-aos="fade-up">
+          I also find joy in learning new languages (besides programming), even
+          if I'm not very good at it, I still find it fun. I'm currently
+          learning Spanish and Korean! Another language I lose myself in is
+          music. These days, I'm honing my skills with the guitar, a journey
+          that truly resonates with my love for learning.
+          <br />
+          To unwind and reconnect with myself, I cherish the simple joys of
+          baking, early morning runs, quality time with loved ones, crocheting
+          and immersing in the serenity of nature.
+        </p>
+
+        <br />
+
+        <p className="text-l leading-relaxed" data-aos="fade-up">
+          Feel free to reach out if you have any questions or just want to chat!
+          I love to connect with like-minded individuals who share common
+          interests and passions.
           <b> Currently seeking Summer 2024 opportunities.</b>
         </p>
       </div>
