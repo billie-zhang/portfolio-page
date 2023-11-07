4c1919c2e11588bff0ee9ef3549091150dde5d6d
Billie Zhang <billie.zhang1504@gmail.com>
2023-11-07 15:53:33 -0500

    add 404 page

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Home from '../pages/Home.jsx';
 import FindEase from '../pages/FindEase.jsx';
 import { Routes, Route } from 'react-router-dom';
+import NoPage from '../pages/NoPage.jsx';
 
 const AnimRoutes = () => {
     return <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/findease' element={<FindEase />} />
+        <Route path='*' element={<NoPage />} />
     </Routes>;
 };
 
---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Contact from "../home/Contact";
+import { Link } from "react-router-dom";
+
+const NoPage = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-full pt-[220px] bg-almost-black text-light-grey"
+    >
+      <div className="max-w-screen-lg p-6 mx-auto flex flex-col justify-center w-full h-auto">
+        <div className="pb-8">
+          <h2 className="font-signature text-5xl font-bold flex items-center justify-center ">
+            Page 404 not found
+          </h2>
+        </div>
+        <Link
+          to={"/"}
+          className="text-3xl font-bold flex items-center justify-center py-4 hover:scale-105 hover:text-medium-blue duration-200 ease-in-out"
+        >
+          Return to the homepage
+        </Link>
+      </div>
+      <Contact />
+    </div>
+  );
+};
+
+export default NoPage;
