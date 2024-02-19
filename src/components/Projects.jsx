4644196ef38fef61f58d0c5025d7c1ebbbb2b5d8
Billie Zhang <billie.zhang1504@gmail.com>
2024-02-19 12:39:43 -0500

    update projects w seasonal

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { MdOpenInNew } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
+import seasonal from "../assets";
 import bzwebsite from "../assets/website.png";
 import bbl from "../assets/bbl.png";
 import eco from "../assets/ecothreads.png";
@@ -15,6 +16,15 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
+      src: seasonal,
+      title: "Seasonal",
+      desc: "A personal portfolio website showcasing my professional journey, skills, and accomplishments. The site serves as a comprehensive snapshot of my abilities.",
+      lang: "react.js, tailwind CSS, OpenAI, Figma",
+      github: "https://github.com/billie-zhang/portfolio",
+      link: "https://billiezhang.vercel.app/",
+    },
+    {
+      id: 2,
       src: bzwebsite,
       title: "Portfolio Website",
       desc: "A personal portfolio website showcasing my professional journey, skills, and accomplishments. The site serves as a comprehensive snapshot of my abilities.",
@@ -23,7 +33,7 @@ const Projects = () => {
       link: "https://billiezhang.vercel.app/",
     },
     {
-      id: 2,
+      id: 3,
       src: eco,
       title: "EcoThreads",
       desc: "A website that displays an interactive Figma prototype of a fashion sustainability app and serves as a web preview for the app, built for HackTheGlobe.",
@@ -32,7 +42,7 @@ const Projects = () => {
       link: "https://billie-zhang.github.io/EcoThreads/",
     },
     {
-      id: 3,
+      id: 4,
       src: bbl,
       title: "Billie's Baking Life",
       desc: "A food blog where I spill the beans (and sugar) on all things delicious, from pretty cakes to the chewiest cookies! I'm the recipe developer, taste-tester and photographer.",
