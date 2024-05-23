386dbea765680c7c3b390a1c11960d1b3ec09a30
Billie Zhang <billie.zhang1504@gmail.com>
2024-05-23 15:05:34 -0400

    update prof experience with current

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,6 +11,19 @@ const Experience = () => {
   const experience = [
     {
       id: 1,
+      experience: "Quality Software Engineer",
+      company: "Omniscient Neurotechnology",
+      time: "May - Aug 2024",
+      project:
+        "Project: Quicktome, a cloud-based tool built for brain health physicians that allows them to obtain visualization of clinically critical brain networks with automated mapping technology.",
+      point1:
+        " - Developed and executed automated test scripts using Playwright, significantly reducing manual testing efforts and enhancing overall test coverage by 88%.",
+      point2:
+        " - Enhanced Quicktome's software quality and reliability as measured by a 30% decrease in bug reports from end-users, by implementing comprehensive automated tests using Playwright and Bruno test frameworks.",
+      point3: " - API tests using Bruno",
+    },
+    {
+      id: 2,
       experience: "Full Stack Developer",
       company: "Royal Bank of Canada",
       time: "Sept - Dec 2023",
@@ -26,7 +39,7 @@ const Experience = () => {
         " - Collaborated in cross-functional teams, utilizing Agile methodologies and Jira, to ensure streamlined project management.",
     },
     {
-      id: 2,
+      id: 3,
       experience: "Technical Systems Analyst",
       company: "Royal Bank of Canada",
       time: "Jan - Apr 2023",
